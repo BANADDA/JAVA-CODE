e9fc412f017ed45b785751360307fbbe637cffcb
BANADDA MUBARAKA <83466862+BANADDA@users.noreply.github.com>
2022-01-22 12:32:08 +0300

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,22 @@
+/*
+simple Calculator
+FILE: Calculator.java
+*/
+package p1;
+public class calculator{
+
+    double add(double op1, double op2){
+		return (op1 + op2);
+	}
+
+    double mult(double op1, double op2){
+	   return (op1 * op2);
+	}
+
+    double exp(double op1, double op2){
+		return Math.pow(op1, op2);
+	}
+
+    void show(int dp){
+	}
+}
---
+++ mainClass.java
@@ -0,0 +1,11 @@
+/*
+Main Class
+FILE:mainClass.java
+*/
+import p1.*;
+public class mainClass{
+	public static void main( String[] args){
+		calculutor simpleCalc = new calculator();
+		system.out.println( simpleCalc.add ( 2, 3 ) );
+	}
+}
